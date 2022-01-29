6cb54f2804686ced58dff36e52a960f5dbd68b08
Neacsu Andreea-Maria <andreeaneacsu33@gmail.com>
2022-01-29 13:00:17 +0200

    add auth manager

---
+++ user/src/api.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const fs = require('fs');
+var error = 'Internal server error';
+
+router.route('/authenticate').post((req,res) => {
+    console.log('Request received for path /authenticate');
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log('body username: ' + username);
+    console.log('body password: ' + password);
+
+    fs.readFile('./users.json', 'utf-8', (err, jsonString) => {
+        if (err) {
+            console.log('Error encountered while reading file users.json: ' + err);
+            res.json(error);
+        } else {
+            let users;
+            try {
+                users = JSON.parse(jsonString);
+            } catch (err) {
+                console.log('Error parsing JSON: ' + err);
+                res.json({error});
+            }
+            let found_password = '';
+            users.forEach( (entry) => {
+                if(username === entry.username){
+                    found_password = entry.password;
+                    console.log(`Username ${entry.username} found`);
+                }
+            });
+            if(found_password === ''){
+                console.log('User not found.');
+                error = "User not found";
+                res.json({error});
+            } else if(found_password === password){
+                console.log('Password is correct.');
+                res.json({username,password});
+            } else{
+                console.log('Wrong password.');
+                error = 'Wrong password';
+                res.json({error});
+            }
+        }
+    });
+});
+
+module.exports = router;
---
+++ user/src/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { route } = require('express/lib/application');
+const userRoute = require('./api');
+
+router.use('/user', userRoute);
+module.exports = router;
